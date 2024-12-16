574ed4988432e480717d3c520eca938e1621cc2d
satyam.kushwaha <satyam.kushwaha@rtctek.com>
2024-12-16 12:43:52 +0530

    implementing thymeLeaf instead of jsp for webview

--- src/main/java/com/webapplication/firstWebApplication/hello/HelloController.java
+++ src/main/java/com/webapplication/firstWebApplication/hello/HelloController.java
@@ -1,13 +1,19 @@
 package com.webapplication.firstWebApplication.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
 
-	
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
 	@RequestMapping("say-hello")
 	@ResponseBody
 	public String sayHello()
@@ -15,9 +21,11 @@ public class HelloController {
 		return "Hello what are you learning today";
 	}
 	
-	@RequestMapping("say-hellojsp")
-	public String sayHelloJsp()
+	@RequestMapping("say-helloweb")
+	public String sayHelloJsp(@RequestParam String name, ModelMap map)
 	{
+ logger.info(name);
+ map.put("name", name);
 		return "sayhello";
 	}
 
