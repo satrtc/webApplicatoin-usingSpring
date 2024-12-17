4d552c24c9a27173ec9f437ec260bfefa91a2070
satyam.kushwaha <satyam.kushwaha@rtctek.com>
2024-12-17 14:55:29 +0530

    adding bootstrap in application for enhanced ui and implementing add more todos

--- src/main/java/com/webapplication/firstWebApplication/todo/TodoController.java
+++ src/main/java/com/webapplication/firstWebApplication/todo/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -21,5 +22,17 @@ public class TodoController {
 		List<Todo> allTodos=todoservice.findByUsername("rtc");
 		map.addAttribute("todos", allTodos);
 		return "todoList";
-	} 
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.GET)
+	public String addTododetails()
+	{
+		return "addtodo";
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.POST)
+	public String addTodo()
+	{
+		return "redirect:todo-list";
+	}
 }
